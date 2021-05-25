0328d19c0f0361b0b8a4e0dddffa42e6159b1f02
Mike Meyer <mfmeyer301@gmail.com>
2021-05-25 14:22:40 +0200

    port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -32,8 +33,8 @@ func main() {
 
 	setupRoutes(app)
 
-	// port := os.Getenv("PORT")
-	err = app.Listen("")
+	port := os.Getenv("PORT")
+	err = app.Listen(":" + port)
 
 	if err != nil {
 		log.Fatal("Error app failed to start")
